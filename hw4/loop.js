39a2e25a6a470037d3e26e5bf99fa14aadcaf2a3
Yevhen Voronkov <vrnkv.art@gmail.com>
2025-09-29 09:41:53 +0300

    fix: remove node_modules and add to .gitignore

--- hw4/loop.js
+++ hw4/loop.js
@@ -1,7 +1,35 @@
+// for
 for (let i = 0; i <= 9; i++) {
-  console.log("i =", i);
+  console.log("for i =", i);
 }
 
+// for -10
 for (let j = 100; j >= 0; j -= 10) {
-  console.log("j =", j);
+  console.log("for j =", j);
+}
+
+// while
+let k = 0;
+while (k < 5) {
+  console.log("while k =", k);
+  k++;
+}
+
+// do...while
+let m = 0;
+do {
+  console.log("do...while m =", m);
+  m++;
+} while (m < 3);
+
+// for...of 
+const arr = ["a", "b", "c"];
+for (const item of arr) {
+  console.log("for...of item =", item);
+}
+
+// for...in 
+const obj = { name: "Eugene", age: 33, city: "Dnipro" };
+for (const key in obj) {
+  console.log("for...in", key, "=", obj[key]);
 }
